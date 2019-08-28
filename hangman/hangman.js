666c7a2946367a690a5a159a45524538250af0b6
Ari <43784159+tawnyblvd@users.noreply.github.com>
2019-08-28 17:20:35 -0400

    Add files via upload
    connecting starting hangman game exercises

---
+++ hangman/hangman.js
@@ -0,0 +1,5 @@
+//Set Up of My Variables
+//bank of possible words to pull from
+let availableArtists = ["basquiat", "warhol", "hopper", "celmins", "smith",
+
+                        "ruscha", "noguchi", "o'keeffe", "oldenburg", "driggs"];

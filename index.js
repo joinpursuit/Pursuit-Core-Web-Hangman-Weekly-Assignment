39c142eec4b397865326f952f93b024fbf6b8e52
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-08-28 16:31:32 -0400

    v.02, planning

--- index.js
+++ index.js
@@ -7,3 +7,24 @@ const debug = (logThis) => {
 
 
 
+/* PlANNING
+
+future its: vs Computer/AI
+
+process:
+Enter Player 1 Name
+Enter Player 2 Name
+Random choose who goes first
+Game Engine
+Save Records
+Loser Starts Next Round?
+Restart Round Mech
+New Match Mech
+
+secondary add-ons:
+Visual Editing
+Sound Editing
+
+
+
+*/

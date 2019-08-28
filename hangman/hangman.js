ffd58e464bf48cb60a3d34a4aa339d823a2da378
tawnyblvd <margara.sees@gmail.com>
2019-08-28 17:45:00 -0400

    test of js start work connects with Github

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -3,3 +3,8 @@
 let availableArtists = ["basquiat", "warhol", "hopper", "celmins", "smith",
 
                         "ruscha", "noguchi", "o'keeffe", "oldenburg", "driggs"];
+//round's current word to guess
+//we are saying to pull a word from the bank, or an index of our word bank array
+//random allows us to randomly pick a word
+//floor ensures the number we get isn't a weird decimal                    
+let currentRoundArtist = availableArtists[Math.floor(Math.random() * availableArtists.length)];

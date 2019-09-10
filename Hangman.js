8c83f59c86d7357f76948b0de2d94d644b18c49d
shaynadavis <shaynadavis@pursuit.org>
2019-09-09 20:51:33 -0400

    Shayna Davis Hangman Project

--- Hangman.js
+++ Hangman.js
@@ -1,58 +1,84 @@
 //creating an addeventlistener to start the game with a randomized word
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("triggered")
+    //console.log("triggered")
         let startGameForm = document.querySelector("#start_game");
-        let startGameFormValue = document.querySelector("#start_game").innerText;
+        //let startGameFormValue = document.querySelector("#start_game").innerText;
         let startButton = document.querySelector("#playerClick")
         console.dir(startGameForm)   
         
         startGameForm.addEventListener("submit", (event) => {
-            event.preventDefault();  
-                // Create an array of words
-                let meanGirlWords = ["fetch", "social suicide", "word vomit", "grool", "pink", "toaster strudel" ];
-                // Pick a random word
-                // console.log(meanGirlWords + "words")
-                let meanGirlWord = meanGirlWords[Math.floor(Math.random() * meanGirlWords.length)];
-                // console.log("meanGirlWord")
-                // Set up the answer array
-                let replacingWordWithUnderscore = []; 
-                const playTheGame = () => {
-                    console.log("triggereddddd") 
-                for (let i = 0; i < meanGirlWord.length; i++) {
-                    replacingWordWithUnderscore[i] = "_";
-                    // console.log("play the game" + playTheGame())
+					event.preventDefault();  
+					// Create an array of words
+					let meanGirlWords = ["fetch", "social suicide", "word vomit", "grool", "pink", "toaster strudel" ];
+					// Pick a random word
+					// console.log(meanGirlWords + "words")
+					let meanGirlWord = meanGirlWords[Math.floor(Math.random() * meanGirlWords.length)];
+					console.log("meanGirlWord", meanGirlWord)
+					// Set up the answer array
+					let replacingWordWithUnderscore = [];
+					
+					const replaceString = () => {
+						for (let i = 0; i < meanGirlWord.length; i++) {
+							replacingWordWithUnderscore.push("_") 
+						}  
+							return replacingWordWithUnderscore.toString()
+					}
+					
+          const playTheGame = () => {
+            for (let i = 0; i < meanGirlWord.length; i++) {
+              replacingWordWithUnderscore.push("_") 
             }  
-                console.log("replacingWordWithUnderscore", replacingWordWithUnderscore)                    
-        } 
+            return replacingWordWithUnderscore.toString()
+            //console.log("replacingWordWithUnderscore", replacingWordWithUnderscore)  
+            // return "Will append?"                  
+				} 
         // let playTheGameVar = playTheGame()
         // console.log(playTheGameVar + "playTheGameVar")
-        let paragraphOne = document.createElement("p")
-        paragraphOne.innerText = playTheGame()
-        console.dir(paragraphOne)
+        let paragraphTag = document.createElement("p")
+        paragraphTag.innerText = playTheGame()
+        console.dir(paragraphTag, "Testing")
         // paragraphOne.appendChild(displayMeanGirlWord);
-        document.body.appendChild(paragraphOne)              
-    })
+        document.body.appendChild(paragraphTag)
+        
+        
+			})
+
+			document.getElementById("userEnterLetterClick").addEventListener("click", function(){
+				event.preventDefault();  
+					let inputtedLetter = document.querySelector("#letter_input");
+					console.log("VALUE:", inputtedLetter.value);
+
+			});
 })
 
-// creating an addeventlistener to create a way for the player to react to the game by entering a letter
-document.addEventListener("DOMContentLoaded", () => {
-    let enterALetterForm = document.querySelector("#enter_letter");
-    let enterLetterButton = document.querySelector("#userEnterLetterClick")
 
-    enterALetterForm.addEventListener("submit", (event) => {
-        //creating a variable for the length of words.
-        let remainingLetters = meanGirlWord.length;
-        const addNewLetter = () => {
-            // the game loop
-            while (remainingLetters > 0) {
-            // Show the player their progress
-            let paragraphTwo = document.createElement("p")
-            replacingWordWithUnderscore.join(" ")
-            document.body.appendChild("paragraghTwo")
-            }
-        }
-    })    
-})     
+//have player guess letter
+//if wrong add to list of guessed letters
+//if correct replace underscore with the letter
+//subtracting amount of guesses the player has
+//if guesses all letter player won
+//if too many wrong guesses player loses
+
+// creating an addeventlistener to create a way for the player to react to the game by entering a letter
+// document.addEventListener("DOMContentLoaded", () => {
+//     let enterALetterForm = document.querySelector("#enter_letter");
+//     let enterLetterButton = document.querySelector("#userEnterLetterClick")
+
+//     enterALetterForm.addEventListener("submit", (event) => {
+//         event.preventDefault()
+//         //creating a variable for the length of words.
+//         let remainingLetters = meanGirlWord.length;
+//         const addNewLetter = () => {
+//             // the game loop
+//             while (remainingLetters > 0) {
+//             // Show the player their progress
+//             let paragraphTwo = document.createElement("p")
+//             replacingWordWithUnderscore.join(" ")
+//             document.body.appendChild("paragraghTwo")
+//             } 
+//         }
+//     })    
+// })     
                 
 //                 // The game loop
 //                 while (remainingLetters > 0) {

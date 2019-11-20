07e8fcf69969eed979ae41ba3385cec1e3dafbca
bsihame <sihamebazi@pursuit.org>
2019-11-20 14:21:30 -0500

    Creat files js css html

--- words.js
+++ words.js
@@ -1,3 +1,14 @@
-let humanBody = ["blood", "body", "cell", "digestion", "disease", "drug", "excrete", "exercise", "face", "foot", "hair", "head", "health", "hygiene", "injury", "muscle", "nerve", "organ", "reproduction", "respiration", "sense", "skeleton", "tooth"];
-let humanMind = [ 'action', 'anger', 'behavior', 'belief', 'care', 'emotion', 'fear', 'happiness', 'hate', 'knowledge', 'learn', 'love', 'mind', 'morals', 'personality', 'sadness', 'thought' ];
-let everydayLife = ['airplane', 'boat', 'building', 'car', 'city', 'clothing', 'community', 'cooking', 'drink', 'family', 'food', 'furniture', 'game', 'home', 'jewelry', 'leisure', "relationship', 'room', 'school', 'sport', 'tool', 'transportation'];
+let humanBody = ['blood', 'body', 'cell', 'digestion', 'disease', 'drug', 'excrete', 'exercise', 'face', 'foot', 'hair', 'head', 'health', 'hygiene', 'injury', 'muscle', 'nerve', 'organ', 'reproduction', 'respiration', 'sense', 'skeleton', 'tooth'];
+let humanMind = ['action', 'anger', 'behavior', 'belief', 'care', 'emotion', 'fear', 'happiness', 'hate', 'knowledge', 'learn', 'love', 'mind', 'morals', 'personality', 'sadness', 'thought' ];
+let everydayLife = ['airplane', 'boat', 'building', 'car', 'city', 'clothing', 'community', 'cooking', 'drink', 'family', 'food', 'furniture', 'game', 'home', 'jewelry', 'leisure', 'relationship', 'room', 'school', 'sport', 'tool', 'transportation'];
+let historyAndCulture = ['celebration', 'culture', 'education', 'history', 'myth', 'religion', 'science', 'tradition'];
+let communication = ['art', 'book', 'color', 'communication', 'dance', 'film', 'grammar', 'instrument', 'language', 'literature', 'music', 'photography', 'theater'];
+
+
+console.log(humanBody)
+
+let humanBodyDefinition = `Words for parts of our bodies, how our bodies work, and how we care for our bodies`;
+let humanMindDefinition = `Words for how we feel, how we think, and how we act`;
+let everydayLifeDefinition = `Words for where we live, how we travel, what we eat and wear, who we know, and how we have fun`;
+let historyAndCulture = `Words for what organizes society, what has happened in the past, and what shapes the future`;
+let communication = `Words for how we communicate with language and how we communicate through art`;

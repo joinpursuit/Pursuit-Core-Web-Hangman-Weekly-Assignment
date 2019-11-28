d7ea76f9c308a411b0e92c2940a3b0f848254c2e
Farrah Isabelle <farrahrios@pursuit.org>
2019-11-27 23:51:50 -0500

    hangman project

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded",()=> {
+
+
+        let newName =(prompt("What is your name"))
+     let alertOne = (alert(newName))
+        alertOne.innerText = newName
+    
+    
+})
